6906ba6c2301d559d5f3dd41f09489fecf687b19
Filip Petkovski <108181951+filipetkovski@users.noreply.github.com>
2022-07-15 12:16:32 +0200

    Add files via upload

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -40,11 +40,13 @@ function menuOpen() {
     const nav1= document.getElementById("nav");
     const ham1 = document.getElementById("ham1");
     const ham2 = document.getElementById("ham2");
-    let navButton1 = document.getElementById("span1");
-    let navButton2 = document.getElementById("span2");
-    let navButton3 = document.getElementById("span3");
-    let navButton4 = document.getElementById("span4");
-    let navButton5 = document.getElementById("span5");
+    let navButton = [];
+
+    navButton[0] = document.getElementById("span1");
+    navButton[1] = document.getElementById("span2");
+    navButton[2] = document.getElementById("span3");
+    navButton[3] = document.getElementById("span4");
+    navButton[4] = document.getElementById("span5");
 
     const dark = document.getElementById("darkTheme");
     dark.style.display = "unset";
@@ -58,23 +60,22 @@ function menuOpen() {
     ham2.style.justifyContent = "center";
     ham2.style.alignItems = "center";
 
-    navButton1.style.fontSize = "1em";
-    navButton2.style.fontSize = "1em";
-    navButton3.style.fontSize = "1em";
-    navButton4.style.fontSize = "1em";
-    navButton5.style.fontSize = "1em";
-
+    for(let i=0;i<5;i++) {
+        navButton[i].style.fontSize = "1em";
+    }
 }
 
 function menuClose() {
     let nav1= document.getElementById("nav");
     let ham1 = document.getElementById("ham1");
     let ham2 = document.getElementById("ham2");
-    let navButton1 = document.getElementById("span1");
-    let navButton2 = document.getElementById("span2");
-    let navButton3 = document.getElementById("span3");
-    let navButton4 = document.getElementById("span4");
-    let navButton5 = document.getElementById("span5");
+    let navButton = [];
+
+    navButton[0] = document.getElementById("span1");
+    navButton[1] = document.getElementById("span2");
+    navButton[2] = document.getElementById("span3");
+    navButton[3] = document.getElementById("span4");
+    navButton[4] = document.getElementById("span5");
 
     const dark = document.getElementById("darkTheme");
     dark.style.display = "none";
@@ -86,10 +87,8 @@ function menuClose() {
     ham1.style.display = "flex";
     ham2.style.display = "none";
 
-    navButton1.style.fontSize = "0";
-    navButton2.style.fontSize = "0";
-    navButton3.style.fontSize = "0";
-    navButton4.style.fontSize = "0";
-    navButton5.style.fontSize = "0";
+    for(let i=0;i<5;i++) {
+        navButton[i].style.fontSize = "0";
+    }
 
 }
